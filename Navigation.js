4dea94004cfb76b9abaf4e541ca3e31a16541611
Ali Qasimzade <qasimzadeali4@gmail.com>
2021-01-13 11:52:03 +0400

    Changed hamburger menu for responsive web (Only CSS file)

--- Navigation.js
+++ Navigation.js
@@ -1,17 +1,19 @@
 const openMenuButton = document.getElementById('openMenuButton');
 const closeMenuButton = document.getElementById('closeMenuButton');
+const menu = document.getElementById("menu");
 let flag = true;
 openMenuButton.addEventListener('click', () => {
   if (flag) {
-    document.getElementById('menu').style.width = '100%';
-    document.getElementById('menu').style.opacity = '1';
+    menu.style.width = '100%';
+    menu.style.opacity = '1';
     flag = false;
   } else {
-    document.getElementById('menu').style.width = '0%';
-    document.getElementById('menu').style.opacity = '0';
+    menu.style.width = '0%';
+    menu.style.opacity = '0';
     flag = true;
   }
 });
-closeMenuButton.addEventListener('click', () => {
-  document.getElementById('menu').style.width = '0%';
+closeMenuButton.addEventListener("click", () => {
+  menu.style.width = "0";
+  menu.style.opacity = "0";
 });
